5a9ed642d10a84b28f18d5a4d98c2068bb75d1aa
Tim McMaster <tim@mcmaster.id.au>
2019-09-28 11:46:01 +1000

    completed the dynamic ratio resizing

--- src/tm-a4-page.js
+++ src/tm-a4-page.js
@@ -1,4 +1,4 @@
-import {html, LitElement} from 'lit-element';
+import {html, css, LitElement} from 'lit-element';
 
 class TmA4Page extends LitElement {
 
@@ -14,19 +14,82 @@ class TmA4Page extends LitElement {
         this.heading = 'Hello World!';
     }
 
+
+    static get styles() {
+        // language=CSS
+        return [css`
+            :host {
+                display: flex;
+                justify-content: center;
+                box-sizing: border-box;
+                background: lightgray;
+                width:100vw;
+                height:100vh;
+                font-size: 2vh;
+            }
+
+            /*noinspection CssUnusedSymbol*/
+            div.body {
+                display: inline-block;
+                box-sizing: border-box;
+                //border: solid grey 2px;
+                background: white;
+            }
+            
+            @media only screen and (orientation: portrait) and (max-aspect-ratio: 10/14) {
+                :host {
+                    flex-direction: column;
+                    //border: solid red 2px;
+                }
+            }
+
+            @media only screen and (orientation: portrait) and (min-aspect-ratio: 10/14), only screen and (orientation: landscape) {
+                :host {
+                    flex-direction: row;
+                    //border: solid blue 2px;
+                }
+            }
+
+            @media only screen and (orientation: portrait) {
+                /*noinspection CssUnusedSymbol*/
+                div.body {
+                    //border: solid blue 2px;
+                    width: 100vw;
+                    height: calc(100vw * 1.4);
+                }
+            }
+
+            @media only screen and (orientation: portrait) and (min-aspect-ratio: 10/14) {
+                /*noinspection CssUnusedSymbol*/
+                div.body {
+                    //border: solid red 2px;
+                    height: 100vh;
+                    width: calc(100vh / 1.4);
+                }
+            }
+
+            @media only screen and (orientation: landscape) {
+                /*noinspection CssUnusedSymbol*/
+                div.body {
+                    //border: solid green 2px;
+                    height: 100vh;
+                    width: calc(100vh / 1.4);
+                }
+            }
+            
+            h3 {
+                text-align: center;
+                margin-top: 20vh;
+                color: grey;
+            }
+        `];
+    }
+
     // noinspection JSUnusedGlobalSymbols
     render() {
         return html`
-      <style>
-        :host {
-          display: inline-block;
-        }
-        h2 {
-            color: gray;
-        }
-      </style>
-      <h2>${this.heading}</h2>
-    `;
+            <div class="body"><h3>This is an A4 page</h3></div>
+        `;
     }
 
 
